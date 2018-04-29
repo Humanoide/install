408a9ee379999d61098e6f57b99f66f772355fef
Fernando García Hansen <fgarcia@humanoide.es>
2018-04-29 11:52:56 +0200

    Update creacertificado.sh

--- creacertificado.sh
+++ creacertificado.sh
@@ -3,4 +3,4 @@
     # Preguntamos al usuario cual es el certificado a crear
     echo Hola, ¿cual es el dominio del certificado?
     read varname
-    echo ./letsencrypt-auto --apache -d $varname
+    echo ./opt/letsencript/letsencrypt-auto --apache -d $varname
